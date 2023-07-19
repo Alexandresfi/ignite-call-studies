d21db3851a5303c1a214940dff90c61a6d45ee3c
Alexandre Nascimento <alexandre.sfi@hotmail.com>
2023-07-19 10:28:09 -0300

    feat: register screen

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -7,6 +7,7 @@ import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 
 import { Form, FormAnnotation } from './styles'
+import { useRouter } from 'next/router'
 
 const claimUsernameFormSchema = z.object({
   username: z
@@ -24,13 +25,16 @@ export function ClaimUsernameForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<claimUsernameFormData>({
     resolver: zodResolver(claimUsernameFormSchema),
   })
 
+  const router = useRouter()
+
   async function handleClainUsername(data: claimUsernameFormData) {
-    console.log(data.username)
+    const { username } = data
+    await router.push(`/register?username=${username}`)
   }
 
   return (
@@ -42,7 +46,7 @@ export function ClaimUsernameForm() {
           placeholder="seu-usuário"
           {...register('username')}
         />
-        <Button size="sm" type="submit">
+        <Button size="sm" type="submit" disabled={isSubmitting}>
           Reservar
           <ArrowRight />
         </Button>
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,8 +1,53 @@
-import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
-import { Container, Form, Header } from './styles'
+import { useForm } from 'react-hook-form'
+import z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { ArrowRight } from 'phosphor-react'
 
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+
+import { Container, Form, FormError, Header } from './styles'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((username) => username.toLocaleLowerCase()),
+
+  name: z.string().min(3, {
+    message: 'O nome do usuário precisa ter pelo menos 3 letras',
+  }),
+})
+
+type RegisterForm = z.infer<typeof registerFormSchema>
+
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterForm>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [setValue, router.query?.username])
+
+  async function handleRegister(data: RegisterForm) {
+    console.log(data)
+  }
+
   return (
     <Container>
       <Header>
@@ -15,18 +60,28 @@ export default function Register() {
         <MultiStep size={4} currentStep={1} />
       </Header>
 
-      <Form as="form">
+      <Form as="form" onSubmit={handleSubmit(handleRegister)}>
         <label htmlFor="">
           <Text size="sm">Nome do usuário</Text>
-          <TextInput prefix="ignite.com/" placeholder="seu usuario" />
+          <TextInput
+            prefix="ignite.com/"
+            placeholder="seu usuario"
+            {...register('username')}
+          />
+          {errors.username && (
+            <FormError size="sm">{errors.username.message}</FormError>
+          )}
         </label>
 
         <label htmlFor="">
           <Text size="sm">Nome completo</Text>
-          <TextInput placeholder="Seu nome" />
+          <TextInput placeholder="Seu nome" {...register('name')} />
+          {errors.username && (
+            <FormError size="sm">{errors.name?.message}</FormError>
+          )}
         </label>
 
-        <Button>
+        <Button disabled={isSubmitting}>
           Próximo passo
           <ArrowRight />
         </Button>
