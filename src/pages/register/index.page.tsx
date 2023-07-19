2ea56abb0041bbe06eb3a08bd5bddc2096253926
Alexandre Nascimento <alexandre.sfi@hotmail.com>
2023-07-19 09:34:31 -0300

    feat:  register page, stap 1

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,36 @@
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { Container, Form, Header } from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+
+      <Form as="form">
+        <label htmlFor="">
+          <Text size="sm">Nome do usuário</Text>
+          <TextInput prefix="ignite.com/" placeholder="seu usuario" />
+        </label>
+
+        <label htmlFor="">
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu nome" />
+        </label>
+
+        <Button>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
