ac2716355e7378d79df9565e6121fa05b0698b29
Alexandre Nascimento <alexandre.sfi@hotmail.com>
2023-07-31 17:05:39 -0300

    feat: fix days month calendar

--- src/pages/schedule/[username]/scheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/scheduleForm/CalendarStep/index.tsx
@@ -7,18 +7,24 @@ import {
   TimePickerList,
 } from './styles'
 import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
   const isDateSelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describeDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            Domingo <span>30 de Julho</span>
+            {weekDay} <span>{describeDate}</span>
           </TimePickerHeader>
           <TimePickerList>
             <TimePickerItem>08:00h</TimePickerItem>
